fc0d0f3d9233d926ae3bcbc8adb6c1214bdaad51
Jason Park <parkjs814@gmail.com>
2017-04-22 22:00:35 -0500

    Yes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 class Card extends React.Component {
   render() {
     return (
-      <div className="card-wrapper">
-        <div className={`card ${this.props.vertical ? 'vertical' : null}`}>
+      <div className="card-wrapper" style={{
+        backgroundColor: this.props.color,
+        color: this.props.white ? 'white' : ''
+      }}>
+        <div className={`card ${this.props.vertical ? 'vertical' : ''}`}>
           {this.props.children}
         </div>
       </div>
